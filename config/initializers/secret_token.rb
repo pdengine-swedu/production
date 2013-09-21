7d58a66ad480249c7338f1a556f84e21abb40b11
Ron Searl <searl134@gmail.com>
2013-09-21 15:47:22 -0500

    check in folders initially gitignored

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = '7fafc6b1d25c0d320d1a0088ede04d70179926608df801be55c961e7f8c0aad31bfca8e0de4390efcb5addc48a7a8d2f38fb5e654cca3176735540ce4a96876b'
