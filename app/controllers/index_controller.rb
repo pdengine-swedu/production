a369aebe22b30d1ba4be91500955468e239057e8
Ron Searl <searl134@gmail.com>
2013-09-21 19:00:13 -0500

    added autocomplete front end to home page

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,20 @@
+class IndexController < ApplicationController
+  before_action :set_seminar, only: []
+
+  # GET /seminars
+  # GET /seminars.json
+  def index
+    @seminars = Seminar.all
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_seminar
+      @seminar = Seminar.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def seminar_params
+      params.require(:seminar).permit(:description, :title)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Myapp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'seminars#index'
+  root 'index#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
