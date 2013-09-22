a710047133d4151ed3bc69f2ed02fb31a82b0e50
Ron Searl <searl134@gmail.com>
2013-09-22 01:22:00 -0500

    modified seeds.rb to read in a csv

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,8 @@
 class Tag
 	include Mongoid::Document
 	field :title, type: String
-	
+
+	index({ title: 1 }, { unique: true, name: "title_index" })
+
 	has_and_belongs_to_many :seminars
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+CSV.foreach("/Users/ron/Documents/tagsTest.csv") do |row|
+	row.each do |tag|
+		Tag.create(title: tag);
+	end
+end
+
+CSV.foreach("/Users/ron/Documents/PD Engine Submissions (Responses) - Form Responses.csv") do |row|
+		isOnline = false;
+		if row[1] != nil && row[1].downcase == 'online'
+			isOnline = true;
+		end
+		isFree = true;
+		if row[2] != nil && row[2].downcase == 'paid'
+			isFree = false;
+		end
+		title = row[3];
+		startDate = row[4];
+		endDate = row[5];
+		instructorName = row[6];
+		twitterHandle = row[7];
+		description = row[8];
+
+		#address
+		lineOne = row[9];
+		city = ''
+		state = '';
+		if row[10] != nil
+			cityState = row[10].split(',');
+			city = cityState[0];
+			if cityState[1] != nil
+				state = cityState[1];
+			end
+		end
+		zip = row[11];
+
+		eventLink = row[12];
+		supportingResourceLink = row[13];
+
+		seminar = Seminar.create(title: title, startDate: startDate, endDate: endDate, instructorName: instructorName,
+		 twitterHandle: twitterHandle, description: description, line_1: lineOne, city: city, state: state, 
+		 zip_code: zip, isOnline: isOnline, isFree: isFree, seminar_link: eventLink, additional_resource_link: supportingResourceLink);
+
+		tags = row[14].split(',');
+		print tags
+		tags.each do |tag|
+			seminar.tags.push(Tag.new(title: tag));
+		end
+		print seminar.tags;
+		seminar.save;
+
+end
